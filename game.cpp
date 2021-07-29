7b9424928b21b92a0da85c5b86e1a31e15edd8db
DDVHegde100 <69629876+DDVHegde100@users.noreply.github.com>
2021-07-29 07:24:10 -0400

    Update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,154 +1,73 @@
 #include <iostream>
-#include <cmath>
-using namespace std;
+#include <string> 
+#include <cstdlib>
+#include <ctime>
 
+using namespace stdl
+void rules();
 
-int main()
+int main(){
+	string playerName;
+   int amount;
+   int bet;
+   int guess;
+   int dice; 
+   char choice;
+   srand(time(0)); 
+   cout << "\n\t\t========Welcome to Dhruv's Tech Support Casino=======\n\n";
+   cout << "\n\nWhat's your Name : ";
+   getline(cin, playerName);
+   cout << "\n\nEnter the starting balance to play game : $";
+   cin >> balance;
+   do{
+	system("cls")
+	rules();
+cout << "\n\n You current balance is $ " << amount << "\n"
+do{
+	cout << "Hey, " << playerName<<", enter amount to bet : $";
+            cin >> bet;
+            if(bet > balance)
+                cout << "Betting balance can't be more than current balance!\n"
+                       <<"\nRe-enter balance\n ";
+        }while(bettingAmount > balance);
+				do
+        {
+            cout << "Guess any betting number between 1 & 10 :";
+            cin >> guess;
+            if(guess <= 0 || guess > 10)
+                cout << "\nPlease enter a number that is between 1 and 10!\n"
+                    <<"Re-enter number:\n ";
+        }while(guess <= 0 || guess > 10);
+        dice = rand()%10 + 1;
+        if(dice == guess)
+        {
+            cout << "\n\nWow! You got lucky and won!" << bettingAmount * 10;
+            balance = balance + bettingAmount * 10;
+        }
+        else
+        {
+            cout << "Sadly, you lost this round."<< bettingAmount <<"\n";
+            balance = balance - bettingAmount;
+        }
+        cout << "\nThe winning number was : " << dice <<"\n";
+        cout << "\n"<<playerName<<", You have balance of $ " << balance << "\n";
+        if(balance == 0)
+        {
+            cout << "You lost all your money! You balance is 0.";
+            break;
+        }
+        cout << "\n\n-->Do you want to play again (Y/N)? ";
+        cin >> choice;
+    }while(choice =='Y'|| choice=='y');
+    cout << "\n\n\n";
+    cout << "\n\nThanks for playing the game. Your balance is now $ " << balance << "\n\n";
+    return 0;
+}
+void rules()
 {
-    int a;
-    int b;
-    int c;
-    int d;
-    int e;
-    int f;
-    int g;
-    int h;
-    int i;
-    int j;
-    int k;
-    int l;
-    int m;
-    int n;
-    int o;
-    int p;
-    int x;
-    int type;
-    int sum;
-    int sub;
-    int prod;
-    int divid;
-    int twoRoot;
-    int threeRoot;
-    int cosine;
-    int sine;
-    int tangent;
-    int invcos;
-    int invsin;
-    int invtan;
-    
-    cout << "Enter 1 for addition. Enter 2 for subtraction. Enter 3 for multiplication. Enter 4 for division. Enter 5 for Square roots. Enter 6 for Cube roots. Enter 7 for cosine. Enter 8 for sine. Enter 9 for tangent. Enter 10 for inverse cosine. Enter 11 for inverse sine. Enter 12 for inverse tangent: ";
-    cin >> x;
-    
-    if(x==1){
-    cout << "Enter the first number! \n";
-    cin >> a;
-    
-    cout << "Enter the second number! \n";
-    cin >> b;
-    
-    sum=a+b;
-    cout << "The sum of those numbers is " <<sum;
-    return 0;
-    }else if(x==2){
-    cout << "Enter the first number for subtraction! \n";
-    cin >> c;
-    
-    cout << "Enter the second number for subtraction! \n";
-    cin >> d;
-    
-    sub=c-d;
-    cout << "The value of those numbers is " <<sub;
-    return 0;
-    }else if(x==3){
-    cout << "Enter the first number for multiplication! \n";
-    cin >> e;
-    
-    cout << "Enter the second number for multiplication! \n";
-    cin >> f;
-    
-    prod=e*f;
-    cout << "The product of those numbers is " <<prod;
-    return 0; 
-    }else if(x==4){
-    cout << "Enter the first number for division! \n";
-    cin >> g;
-    
-    cout << "Enter the second number for division! \n";
-    cin >> h;
-    
-    divid=g/h;
-    cout << "The dividend of those numbers is " <<divid;
-    return 0; 
-    }else if(x==5){
-        
-    cout << "Enter the first number for it's root! \n";
-    cin >> i;
-    
-    twoRoot=sqrt(i);
-    cout << "The root of the number is " <<twoRoot;
-        
-    }else if(x==6){
-        
-    cout << "Enter the first number for it's cube root! \n";
-    cin >> j;
-    
-    threeRoot=cbrt(j);
-    cout << "The cube root of the number is " <<threeRoot;
-        
-    }else if(x==7){
-        
-    cout << "Enter the first number for it's cosine value! \n";
-    cin >> k;
-    
-    cosine=cos(k);
-    cout << "The cosine value of the number is " <<cosine;
-        
-    }else if(x==8){
-        
-    cout << "Enter the first number for it's sine value! \n";
-    cin >> l;
-    
-    sine=sin(l);
-    cout << "The sine value of the number is " <<sine;
-        
-    }else if(x==9){
-        
-    cout << "Enter the first number for it's tangent value! \n";
-    cin >> m;
-    
-    tangent=tan(m);
-    cout << "The tangent value of the number is " <<tangent;
-        
-    }else if(x==10){
-        
-    cout << "Enter the number\n";
-    cin >> n;
-    
-    invcos=acos(n);
-    cout << "The inverse cosine value of the number is " <<invcos;
-        
-    }else if(x==11){
-        
-    cout << "Enter the number\n";
-    cin >> o;
-    
-    invsin=asin(o);
-    cout << "The inverse sine value of the number is " <<invsin;
-        
-    }else if(x==12){
-        
-    cout << "Enter the number\n";
-    cin >> p;
-    
-    invtan=atan(p);
-    cout << "The inverse tangent value of the number is " <<invtan;
-        
-    }else if(x>12){
-        cout << "Please enter a number between 1-4.";
-    }
-    
-    
-    
-    
-    
+    system("cls");
+    cout << "\t\t======CASINO NUMBER GUESSING RULES!======\n";
+    cout << "\t1. Choose a number between 1 to 10\n";
+    cout << "\t2. Winner gets 10 times of the money bet\n";
+    cout << "\t3. Wrong bet, and you lose the amount you bet\n\n";
 }
